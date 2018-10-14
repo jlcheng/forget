10238e42a5a5cce004f5195804be7953c10a3e07
John Cheng <johnlicheng@gmail.com>
2018-10-14 12:47:02 -0700

    plan: design how to efficiently batch index operations

--- db/buffer.go
+++ db/buffer.go
@@ -1,55 +1,10 @@
 package db
 
-import "fmt"
+type BatchBuffer struct {
+	// notifications
+	arrivals chan(struct{})
+	finish   chan(struct{})
 
-type IndexBuffer struct {
-	buf []Note
-	notes chan(Note)
-	stop chan(bool)
-
-	maxBufSize int
-	processed int
-}
-
-func (b IndexBuffer) StartProcessing() {
-	go func() {
-		fmt.Println("start")
-		for {
-			select {
-			case n := <- b.notes:
-				b.buf = append(b.buf, n)
-				if len(b.buf) > b.maxBufSize {
-					b.ProcessBuffer()
-				}
-			case v := <- b.stop:
-				fmt.Println("stop received", v)
-				//b.stop = nil
-				break
-			default:
-				fmt.Println("default in select")
-				break
-			}
-			fmt.Println("outside select")
-		}
-		fmt.Println("here")
-		b.ProcessBuffer()
-	}()
-}
-
-func (b IndexBuffer) ProcessBuffer() {
-	//fmt.Println("buf.size", len(b.buf))
-	for _, note := range b.buf {
-		fmt.Println("indexing", note.ID)
-		b.processed =+ 1
-	}
-	b.buf = b.buf[:0]
-}
-
-func (b IndexBuffer) SendNote(note Note) {
-	select {
-	case b.notes <- note:
-		fmt.Println("note sent")
-	default:
-		fmt.Println("cannot send note")
-	}
+	buffer []struct{}
+	begin func([]struct{})
 }
