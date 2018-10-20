12b433fab756da782d99f8b9b5511083e81de4d7
John Cheng <johnlicheng@gmail.com>
2018-10-19 21:36:19 -0700

    wip: misc cleanup

--- Makefile
+++ Makefile
@@ -3,14 +3,17 @@ GOGEN=go generate
 
 all: generate build
 
-generate:
+generate: trace/loglevel_string.go
+trace/loglevel_string.go:
 	$(GOGEN) trace/log.go
 
-build: 4gt
-
-
-4gt:
+build: out/4gt
+out/4gt:
 	$(GOBUILD) -o out/4gt main.go
 
+.PHONY: clean
 clean:
 	rm -rf out
+
+install: all
+	mv out/4gt $(HOME)/bin
---
+++ testkit/test_helpers.go
@@ -0,0 +1,32 @@
+package testkit
+
+import (
+	"github.com/blevesearch/bleve"
+	"github.com/blevesearch/bleve/index/scorch"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TempIndex() (bleve.Index, error) {
+	tmpDir := filepath.Join(os.TempDir(), "forget-test")
+	err := os.RemoveAll(tmpDir)
+	if err != nil {
+		return nil, err
+	}
+	index, err := bleve.NewUsing(tmpDir, bleve.NewIndexMapping(), scorch.Name, scorch.Name, nil)
+	if err != nil {
+		return nil, err
+	}
+	return index, nil
+}
+
+func CleanUpIndex(t *testing.T, index bleve.Index) {
+	if index == nil {
+		return
+	}
+	err := index.Close()
+	if err != nil {
+		t.Error(err)
+	}
+}
