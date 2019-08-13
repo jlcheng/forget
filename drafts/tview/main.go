a468e9b130fd068bb0cb7c5f776432111ba9c93f
John Cheng <johnlicheng@gmail.com>
2019-08-13 08:44:54 -0700

    wip

--- drafts/tview/main.go
+++ drafts/tview/main.go
@@ -1,17 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jlcheng/forget/db"
 	"github.com/rivo/tview"
-	"fmt"
 )
 
+// TviewUI has a search field and displays the search results
+//
+// Logically it contains two elements: A search input and a display
+// for the results.
+//
+// The SearchInput exposes the current query and takes a callback
+// named 'OnExecute'.
+//
+// The ResultDisplay renders a search result. The search result is
+// pageable and contains snippet, relevance, and file path to each
+// result. It receives a bleve.SearchResult object and renders it.
+//
+//
+// type SearchResult struct {
+// 	Status   *SearchStatus                  `json:"status"`
+// 	Request  *SearchRequest                 `json:"request"`
+// 	Hits     search.DocumentMatchCollection `json:"hits"`
+// 	Total    uint64                         `json:"total_hits"`
+// 	MaxScore float64                        `json:"max_score"`
+// 	Took     time.Duration                  `json:"took"`
+// 	Facets   search.FacetResults            `json:"facets"`
+// }
+//
 type TviewUI struct {
-	app     *tview.Application
+	app *tview.Application
+}
+
+func NewTviewUI() *TviewUI {
+	ui := new(TviewUI)
+	ui.app = tview.NewApplication()
+	return ui
+}
+
+func (ui *TviewUI) Run() {
+	ui.app.Run()
 }
 
 func main() {
 	foo := db.Atlas{}
-	bar := TviewUI{}
-	fmt.Println(foo, bar)
+	bar := NewTviewUI()
+	bar.Run()
 }
